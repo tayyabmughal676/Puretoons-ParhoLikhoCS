4854a7d3491bc638b38ba54d24798994f5fb5144
tayyabmughal676 <tayyabmughal676@gmail.com>
2020-11-06 09:39:21 +0500

    A couple changing.

--- app/src/main/java/com/example/mrtayyab/uogis/StartActivity.kt
+++ app/src/main/java/com/example/mrtayyab/uogis/StartActivity.kt
@@ -9,16 +9,10 @@ import android.content.Intent
 import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.net.ConnectivityManager
-import android.net.NetworkInfo
 import android.net.Uri
 import android.os.Build
 import android.os.Bundle
 import android.os.Environment
-import android.support.design.widget.BottomNavigationView
-import android.support.v4.app.ActivityCompat
-import android.support.v7.app.AlertDialog
-import android.support.v7.app.AppCompatActivity
-import android.support.v7.widget.Toolbar
 import android.text.SpannableString
 import android.text.method.LinkMovementMethod
 import android.text.util.Linkify
@@ -32,18 +26,19 @@ import android.webkit.WebViewClient
 import android.widget.ProgressBar
 import android.widget.TextView
 import android.widget.Toast
-import com.example.mrtayyab.uogis.R.id.info
-import kotlinx.android.synthetic.main.activity_start.*
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.app.ActivityCompat
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
-class StartActivity : AppCompatActivity() {
-
-
-    lateinit var  mWebView : WebView
-    lateinit var mProgressBar : ProgressBar
-    val MY_PERMISSION_REQUEST_CODE = 123
-     lateinit var listItems : Array<String>
-    lateinit var categoryList : Array<String>
-
+open class StartActivity : AppCompatActivity() {
+    lateinit var mWebView: WebView
+    lateinit var mProgressBar: ProgressBar
+    private val MY_PERMISSION_REQUEST_CODE = 123
+    lateinit var listItems: Array<String>
+    lateinit var categoryList: Array<String>
+    lateinit var mBottomNavigationOption: BottomNavigationView
 
     @SuppressLint("SetJavaScriptEnabled")
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -53,24 +48,24 @@ class StartActivity : AppCompatActivity() {
         val toolbar = findViewById<View>(R.id.toolbar) as Toolbar
         setSupportActionBar(toolbar);
 
-        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener)
-
+        mBottomNavigationOption = findViewById(R.id.layoutBottomNavigationView)
+        mBottomNavigationOption.setOnNavigationItemSelectedListener(
+            mOnNavigationItemSelectedListener
+        )
         listItems = resources.getStringArray(R.array.shopping_item)
-        mWebView =findViewById(R.id.myWebView)
+
+        mWebView = findViewById(R.id.myWebView)
         mProgressBar = findViewById(R.id.progressBar)
         mProgressBar.max = 100
 
         checkPermission()
-        val myUrl = "https://www.puretoons.com/index.xhtml"
-
+        val myUrl = "https://www.puretoons.me/"
         checkConnectivity()
-
-
         optionLinkOpen(myUrl)
 
         mWebView.setDownloadListener { url, userAgent, contentDisposition, mimetype, contentLength ->
 
-            if(mWebView.url.contains(".3gp") || mWebView.url.contains(".mp4")){
+            if (mWebView.url!!.contains(".3gp") || mWebView.url!!.contains(".mp4")) {
 //                val i = Intent(Intent.ACTION_VIEW)
 //                i.data = Uri.parse(url)
 //                startActivity(i)
@@ -78,8 +73,6 @@ class StartActivity : AppCompatActivity() {
 //                var videoPath = URLUtil.guessUrl()
 
                 val videoPath = URLUtil.guessFileName(url, contentDisposition, mimetype)
-
-
                 val mBuilder = AlertDialog.Builder(this)
                 mBuilder.setTitle("Choose an item")
                 mBuilder.setSingleChoiceItems(listItems, -1, DialogInterface.OnClickListener { dialogInterface, i ->
@@ -88,22 +81,16 @@ class StartActivity : AppCompatActivity() {
                     if (listItems[i] == "online") {
                         Toast.makeText(this, "Your Choice is Online", Toast.LENGTH_SHORT).show()
 
-
-
                         val i = Intent(Intent.ACTION_VIEW)
                         i.data = Uri.parse(url)
                         startActivity(i)
-
 
 //                        val intent = Intent(applicationContext, VideoActivity::class.java)
 //                        intent.putExtra("videoPath", videoPath)
 //                        startActivity(intent)
 
-
-
                     } else if (listItems[i] == "download") {
                         Toast.makeText(this, "Your Choice is download", Toast.LENGTH_SHORT).show()
-
 
                         val fileName = URLUtil.guessFileName(url, contentDisposition, mimetype) //returns a string of the name of the file THE IMPORTANT PART
 
@@ -117,13 +104,11 @@ class StartActivity : AppCompatActivity() {
                         myManager.enqueue(myRequest)
                         myDownloadingAlert(fileName)
                         Toast.makeText(this, "Your file is Downloading... ${fileName}", Toast.LENGTH_SHORT).show()
-
                     }
                     dialogInterface.dismiss()
                 })
                 val mDialog = mBuilder.create()
                 mDialog.show()
-
             }
         }
 
@@ -143,87 +128,85 @@ class StartActivity : AppCompatActivity() {
     private fun checkConnectivity(): Int {
         var enabled = true
 
-        var internet =2
+        var internet = 2
         val connectivityManager = this.getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
         val info = connectivityManager.activeNetworkInfo
 
         if (info == null || !info.isConnected || !info.isAvailable) {
-             internet = 0//sin conexion
+            internet = 0//sin conexion
             Toast.makeText(applicationContext, "Internet connection is not available...", Toast.LENGTH_SHORT).show()
 
             val builder = AlertDialog.Builder(this)
             builder.setTitle(R.string.app_name)
             builder.setIcon(R.mipmap.ic_launcher)
-            builder.setMessage("Internet connection is not available " )
+            builder.setMessage("Internet connection is not available ")
                     .setCancelable(false)
-                    .setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, id -> finish() })
-                    .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, id -> dialog.cancel() })
+                    .setPositiveButton("Yes", DialogInterface.OnClickListener { _, _ -> finish() })
+                    .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, _ -> dialog.cancel() })
             val alert = builder.create()
             alert.show()
-
             enabled = false
         } else {
-             internet = 1//conexión
+            internet = 1//conexión
             Toast.makeText(applicationContext, "Internet is available...", Toast.LENGTH_SHORT).show()
-
         }
-
         return internet
     }
 
-    fun linkPassing(){
+    private fun linkPassing() {
 
         val CartoonSeriesHindiDubbed = " http://puretoons.com/site_cartoon_series_hindi_dubbed.xhtml"
-        val  CartoonSeriesBanglaDubbed = "http://puretoons.com/site_4.xhtml"
+        val CartoonSeriesBanglaDubbed = "http://puretoons.com/site_4.xhtml"
         val CartoonSeriesEnglishDubbed = "http://puretoons.com/site_347.xhtml"
-        val AnimeSeriesHindiSubbed  = "http://puretoons.com/site_anime_series_hindi_subbed.xhtml"
-        val  CartoonAnimatedMovies = "http://puretoons.com/site_cartoon_animated_movies.xhtml"
-        val  AnimeCartoonSpecialEpisode = "http://puretoons.com/site_cartoon_and_anime_special_episodes.xhtml"
+        val AnimeSeriesHindiSubbed = "http://puretoons.com/site_anime_series_hindi_subbed.xhtml"
+        val CartoonAnimatedMovies = "http://puretoons.com/site_cartoon_animated_movies.xhtml"
+        val AnimeCartoonSpecialEpisode = "http://puretoons.com/site_cartoon_and_anime_special_episodes.xhtml"
 
-        val  FanHindiDubbedVideos = "http://puretoons.com/site_fan_hindi_dubbed.xhtml"
-        val  AnimeCartoonSongs  = "http://puretoons.com/site_anime_cartoon_songs.xhtml"
+        val FanHindiDubbedVideos = "http://puretoons.com/site_fan_hindi_dubbed.xhtml"
+        val AnimeCartoonSongs = "http://puretoons.com/site_anime_cartoon_songs.xhtml"
 
         categoryList = resources.getStringArray(R.array.cartoonsCatergory)
 
-       val mBuilder = AlertDialog.Builder(this)
+        val mBuilder = AlertDialog.Builder(this)
         mBuilder.setTitle("Choose a Category")
         mBuilder.setSingleChoiceItems(categoryList, -1, DialogInterface.OnClickListener { dialogInterface, i ->
             // mResult.setText(listItems[i])
 
-            if (categoryList[i] == "Cartoon Series Hindi Dubbed") {
-//                Toast.makeText(applicationContext , "you select ${myHindi}" , Toast.LENGTH_SHORT).show()
-                optionLinkOpen(CartoonSeriesHindiDubbed)
+            when {
+                categoryList[i] == "Cartoon Series Hindi Dubbed" -> {
+    //                Toast.makeText(applicationContext , "you select ${myHindi}" , Toast.LENGTH_SHORT).show()
+                    optionLinkOpen(CartoonSeriesHindiDubbed)
 
-            } else if (categoryList[i] == "Cartoon Series Bangla Dubbed") {
+                }
+                categoryList[i] == "Cartoon Series Bangla Dubbed" -> {
 
-//                Toast.makeText(applicationContext , "you select ${myAnime}" , Toast.LENGTH_SHORT).show()
-                optionLinkOpen(CartoonSeriesBanglaDubbed)
-            }
-            else if (categoryList[i] == "Cartoon Series English Dubbed") {
+    //                Toast.makeText(applicationContext , "you select ${myAnime}" , Toast.LENGTH_SHORT).show()
+                    optionLinkOpen(CartoonSeriesBanglaDubbed)
+                }
+                categoryList[i] == "Cartoon Series English Dubbed" -> {
 
-//                Toast.makeText(applicationContext , "you select ${myAnimeMovie}" , Toast.LENGTH_SHORT).show()
-                optionLinkOpen(CartoonSeriesEnglishDubbed)
-            }
-            else if (categoryList[i] == "Special Anime Cartoons") {
+    //                Toast.makeText(applicationContext , "you select ${myAnimeMovie}" , Toast.LENGTH_SHORT).show()
+                    optionLinkOpen(CartoonSeriesEnglishDubbed)
+                }
+                categoryList[i] == "Special Anime Cartoons" -> {
 
-//                Toast.makeText(applicationContext , "you select ${mySpecialAnime}" , Toast.LENGTH_SHORT).show()
-                optionLinkOpen(AnimeSeriesHindiSubbed)
-            }
+    //                Toast.makeText(applicationContext , "you select ${mySpecialAnime}" , Toast.LENGTH_SHORT).show()
+                    optionLinkOpen(AnimeSeriesHindiSubbed)
+                }
+                categoryList[i] == "Cartoon/Animated Movies" -> {
 
-            else if (categoryList[i] == "Cartoon/Animated Movies") {
-
-//                Toast.makeText(applicationContext , "you select ${mySuperDragon}" , Toast.LENGTH_SHORT).show()
-                optionLinkOpen(CartoonAnimatedMovies)
-            }
-
-            else if(categoryList[i] == "Anime Cartoon Special Episode"){
-                optionLinkOpen(AnimeCartoonSpecialEpisode)
-            }
-            else if(categoryList[i] == "Fan Hindi Dubbed Videos"){
-                optionLinkOpen(FanHindiDubbedVideos)
-            }
-            else if(categoryList[i] == "Anime Cartoon Songs"){
-                optionLinkOpen(AnimeCartoonSongs)
+    //                Toast.makeText(applicationContext , "you select ${mySuperDragon}" , Toast.LENGTH_SHORT).show()
+                    optionLinkOpen(CartoonAnimatedMovies)
+                }
+                categoryList[i] == "Anime Cartoon Special Episode" -> {
+                    optionLinkOpen(AnimeCartoonSpecialEpisode)
+                }
+                categoryList[i] == "Fan Hindi Dubbed Videos" -> {
+                    optionLinkOpen(FanHindiDubbedVideos)
+                }
+                categoryList[i] == "Anime Cartoon Songs" -> {
+                    optionLinkOpen(AnimeCartoonSongs)
+                }
             }
 
             dialogInterface.dismiss()
@@ -232,13 +215,8 @@ class StartActivity : AppCompatActivity() {
         mDialog.show()
     }
 
-
-
-
-
     @SuppressLint("SetJavaScriptEnabled")
-
-    fun optionLinkOpen(my : String){
+    fun optionLinkOpen(my: String) {
         mWebView.loadUrl(my)
 
         mWebView.settings.javaScriptEnabled = true
@@ -264,31 +242,28 @@ class StartActivity : AppCompatActivity() {
     }
 
     override fun onBackPressed() {
-        if(mWebView.canGoBack()) {
+        if (mWebView.canGoBack()) {
             mWebView.goBack();
         } else {
             super.onBackPressed();
         }
     }
 
-    fun myDownloadingAlert(filename : String ){
+    private fun myDownloadingAlert(filename: String) {
         val builder = AlertDialog.Builder(this)
         builder.setTitle(R.string.app_name)
         builder.setIcon(R.mipmap.ic_launcher)
-        builder.setMessage("Your Downloading is start ${filename}" )
+        builder.setMessage("Your Downloading is start ${filename}")
                 .setCancelable(false)
                 //.setPositiveButton("Yes", DialogInterface.OnClickListener { dialog, id -> finish() })
                 .setNegativeButton("Cancel", DialogInterface.OnClickListener { dialog, id -> dialog.cancel() })
         val alert = builder.create()
         alert.show()
-
     }
 
-    fun alertAbout(){
-
+    private fun alertAbout() {
         val stMyWeb = SpannableString("http://www.youtube.com/parholikhocs")
         Linkify.addLinks(stMyWeb, Linkify.ALL)
-
         val aboutDialog = AlertDialog.Builder(this)
                 .setIcon(R.mipmap.ic_launcher)
                 .setTitle("Puretoons.com Power by Parho Likho CS ")
@@ -297,17 +272,12 @@ class StartActivity : AppCompatActivity() {
                     // TODO Auto-generated method stub
                 }
                 .create()
-
         aboutDialog.show()
-
         (aboutDialog.findViewById<View>(android.R.id.message) as TextView).movementMethod = LinkMovementMethod.getInstance()
-
     }
 
     fun onForwardPressed() {
-
         if (mWebView.canGoForward()) {
-
             mWebView.goForward()
         } else {
             Toast.makeText(this, "Can't go further !", Toast.LENGTH_SHORT).show()
@@ -325,7 +295,7 @@ class StartActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.about -> {
                 alertAbout()
-                Toast.makeText(applicationContext, "Puretoons.com" , Toast.LENGTH_LONG).show()
+                Toast.makeText(applicationContext, "Puretoons.com", Toast.LENGTH_LONG).show()
             }
             else -> return super.onOptionsItemSelected(item)
         }
@@ -335,14 +305,14 @@ class StartActivity : AppCompatActivity() {
     private val mOnNavigationItemSelectedListener = BottomNavigationView.OnNavigationItemSelectedListener { item ->
         when (item.itemId) {
             R.id.menu_home -> {
-                Toast.makeText(applicationContext , "Home " , Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "Home ", Toast.LENGTH_SHORT).show()
                 val myHome = "https://www.puretoons.com/index.xhtml"
                 optionLinkOpen(myHome)
                 return@OnNavigationItemSelectedListener true
             }
 
             R.id.menu_back -> {
-                Toast.makeText(applicationContext , "Press Back " , Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "Press Back ", Toast.LENGTH_SHORT).show()
                 onBackPressed()
                 return@OnNavigationItemSelectedListener true
             }
@@ -352,12 +322,12 @@ class StartActivity : AppCompatActivity() {
                 return@OnNavigationItemSelectedListener true
             }
             R.id.menu_refresh -> {
-                Toast.makeText(applicationContext , "Press Refresh " , Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "Press Refresh ", Toast.LENGTH_SHORT).show()
                 mWebView.reload()
                 return@OnNavigationItemSelectedListener true
             }
             R.id.menu_option -> {
-                Toast.makeText(applicationContext , "categories " , Toast.LENGTH_SHORT).show()
+                Toast.makeText(applicationContext, "categories ", Toast.LENGTH_SHORT).show()
 //                mWebView.reload()
                 linkPassing()
 
@@ -369,8 +339,7 @@ class StartActivity : AppCompatActivity() {
     }
 
 
-
-    protected fun checkPermission() {
+    private fun checkPermission() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
             if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 if (shouldShowRequestPermissionRationale(Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
--- app/src/main/java/com/example/mrtayyab/uogis/VideoActivity.kt
+++ app/src/main/java/com/example/mrtayyab/uogis/VideoActivity.kt
@@ -3,19 +3,20 @@ package com.example.mrtayyab.uogis
 import android.app.ProgressDialog
 import android.media.MediaPlayer
 import android.net.Uri
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.v7.widget.Toolbar
 import android.util.Log
 import android.view.View
 import android.widget.MediaController
 import android.widget.VideoView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 
 class VideoActivity : AppCompatActivity() {
     lateinit var mVideoView: VideoView
-    lateinit var  player: MediaPlayer
+    lateinit var player: MediaPlayer
     lateinit var mediaController: MediaController
     lateinit var pDialog: ProgressDialog
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,12 +24,11 @@ class VideoActivity : AppCompatActivity() {
 
         val toolbar = findViewById<View>(R.id.myToolbar) as Toolbar
         setSupportActionBar(toolbar);
+
         mediaController = MediaController(this)
         mVideoView = findViewById(R.id.myVideoView2)
 
         val videolink = intent.extras!!.getString("videoPath")
-
-
 
         pDialog = ProgressDialog(this)
 //        // Set progressbar title
@@ -51,7 +51,7 @@ class VideoActivity : AppCompatActivity() {
             mVideoView.setVideoURI(video)
 
         } catch (e: Exception) {
-            Log.e("Error", e.message)
+            e.message?.let { Log.e("Error", it) }
             e.printStackTrace()
         }
 
